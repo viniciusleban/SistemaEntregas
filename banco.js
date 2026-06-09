887ca381f09176fd32aac65680a096ecf344eaa6
Vinicius Leban <barvile6@gmail.com>
2026-06-09 20:16:53 -0300

    Remove arquivos duplicados da raiz

--- banco.js
+++
@@ -1,9 +0,0 @@
-const { Sequelize } = require('sequelize');
-
-const sequelize = new Sequelize('banco1', 'postgres', 'postgres', {
-  host: 'localhost',
-  dialect: 'postgres',
-  logging: false,
-});
-
-module.exports = sequelize;
